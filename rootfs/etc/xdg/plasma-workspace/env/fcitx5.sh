d86b39de4089ffd69ef2c7c1449b46727c66f680
Chenx Dust <chenx_dust@outlook.com>
2024-04-29 16:25:47 +0000

    fix fcitx5

--- rootfs/etc/xdg/plasma-workspace/env/fcitx5.sh
+++ rootfs/etc/xdg/plasma-workspace/env/fcitx5.sh
@@ -1,6 +1,8 @@
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
+if [ '$XDG_SESSION_TYPE' != 'wayland' ]; then
+  export GTK_IM_MODULE=fcitx
+  export QT_IM_MODULE=fcitx
+  export SDL_IM_MODULE=fcitx
+fi;
 export XMODIFIERS=@im=fcitx
 export INPUT_METHOD=fcitx
-export SDL_IM_MODULE=fcitx
 export GLFW_IM_MODULE=ibus
