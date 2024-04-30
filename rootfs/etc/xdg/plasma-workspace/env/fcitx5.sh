8081dac8b044a8be13c4c87a7ce59565a21b3345
Chenx Dust <chenx_dust@outlook.com>
2024-04-30 07:27:53 +0000

    fix input

--- rootfs/etc/xdg/plasma-workspace/env/fcitx5.sh
+++ rootfs/etc/xdg/plasma-workspace/env/fcitx5.sh
@@ -1,8 +1,2 @@
-if [ '$XDG_SESSION_TYPE' != 'wayland' ]; then
-  export GTK_IM_MODULE=fcitx
-  export QT_IM_MODULE=fcitx
-  export SDL_IM_MODULE=fcitx
-fi;
 export XMODIFIERS=@im=fcitx
 export INPUT_METHOD=fcitx
-export GLFW_IM_MODULE=ibus
